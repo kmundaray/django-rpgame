d21b60f6642188028e6eadfb2ee59866f3aa120b
Karls Mundaray <kmundaray@gmail.com>
2024-04-04 13:02:23 -0500

    small order change to settings

--- rpproject/settings.py
+++ rpproject/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMP_DIR = os.path.join(BASE_DIR, 'rpgame')
-print("TEMP_DIR", TEMP_DIR)
+TEMPLATE_DIR = os.path.join(TEMP_DIR,'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -69,9 +69,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "rpproject.urls"
-
-TEMPLATE_DIR = os.path.join(TEMP_DIR,'templates')
-print("TEMPLATE_DIR", TEMPLATE_DIR)
 
 TEMPLATES = [
     {
